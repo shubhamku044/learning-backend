4ab6d13aca881cca5ead97b94f6b67dfcd0b1756
Shubham Sharma <shubhamku044@gmail.com>
2022-03-30 00:44:12 +0530

    completed the tour

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,13 +1,20 @@
 const Tour = require('./../models/tourModel')
 
-exports.getAllTours = async (req, res) => {
-	try {
-		console.log(req.query)
+exports.aliasTopTours = (req, res, next) => {
+	req.query.limit = '5'
+	req.query.sort = '-ratingsAverage,price'
+	req.query.fields = 'name,price,ratingsAverage,summary,difficulty'
+	next()
+}
 
-		// BUILD QUERY
+class APIFeatures {
+	constructor(query, queryString) {
+		this.query = query
+		this.queryString = queryString
+	}
 
-		// 1a. Filtering
-		const queryObj = { ...req.query }
+	filter() {
+		const queryObj = { ...this.queryString }
 		const excludedFields = ['page', 'sort', 'limit', 'fields']
 		excludedFields.forEach((el) => delete queryObj[el])
 
@@ -17,16 +24,48 @@ exports.getAllTours = async (req, res) => {
 
 		// console.log(JSON.parse(queryStr))
 
-		let query = Tour.find(JSON.parse(queryStr))
+		// let query = Tour.find(JSON.parse(queryStr))
+		this.query.find(JSON.parse(queryStr))
+	}
+
+	sort() {
+		if (this.queryString.sort) {
+			const sortBy = this.queryString.sort.split(',').join(' ')
+			// console.log(sortBy)
+			this.query = this.query.sort(sortBy)
+		} else {
+			this.query = this.query.sort('-createdAt')
+		}
+	}
+}
+
+exports.getAllTours = async (req, res) => {
+	try {
+		console.log(req.query)
+
+		// BUILD QUERY
+
+		// 1a. Filtering
+		// const queryObj = { ...req.query }
+		// const excludedFields = ['page', 'sort', 'limit', 'fields']
+		// excludedFields.forEach((el) => delete queryObj[el])
+
+		// // 1b. Advance Filtering
+		// let queryStr = JSON.stringify(queryObj)
+		// queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`)
+
+		// // console.log(JSON.parse(queryStr))
+
+		// let query = Tour.find(JSON.parse(queryStr))
 
 		// 2. Sorting
-		if (req.query.sort) {
-			const sortBy = req.query.sort.split(',').join(' ')
-			// console.log(sortBy)
-			query = query.sort(req.query.sort)
-		} else {
-			query = query.sort('-createdAt')
-		}
+		// if (req.query.sort) {
+		// 	const sortBy = req.query.sort.split(',').join(' ')
+		// 	// console.log(sortBy)
+		// 	query = query.sort(req.query.sort)
+		// } else {
+		// 	query = query.sort('-createdAt')
+		// }
 
 		// 3. Field Limiting
 		if (req.query.fields) {
@@ -49,7 +88,8 @@ exports.getAllTours = async (req, res) => {
 		}
 
 		// Execute Query
-		const tours = await query
+		const features = new APIFeatures(Tour.find(), req.query).filter().sort()
+		const tours = await features.query
 
 		res.status(200).json({
 			status: 'success',
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const tourControllers = require('./../controllers/tourController');
+const express = require('express')
+const tourControllers = require('./../controllers/tourController')
 
-const router = express.Router();
+const router = express.Router()
+
+router
+	.route('/top-5-cheap')
+	.get(tourControllers.aliasTopTours, tourControllers.getAllTours)
 
 router
 	.route('/')
 	.get(tourControllers.getAllTours)
-	.post(tourControllers.createTour);
+	.post(tourControllers.createTour)
 router
 	.route('/:id')
 	.get(tourControllers.getTour)
 	.patch(tourControllers.updateTour)
-	.delete(tourControllers.deleteTour);
+	.delete(tourControllers.deleteTour)
 
-module.exports = router;
+module.exports = router
